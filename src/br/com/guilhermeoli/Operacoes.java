2cc4997480cc29108f2e45200d88ff6ec8223c10
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-02-14 14:24:40 -0300

    first commit

---
+++ src/br/com/guilhermeoli/Operacoes.java
@@ -0,0 +1,26 @@
+package br.com.guilhermeoli;
+
+/**
+ * @author guiol
+ */
+public class Operacoes {
+
+    public static void main(String[] args) {
+        operacoesAritimeticas();
+    }
+
+    public static void operacoesAritimeticas() {
+        int num1 = 10;
+        int num2 = 8;
+        int num3 = 6;
+        int num4 = 8;
+
+        int media = (num1 + num2 + num3 + num4) / 4;
+
+
+
+        System.out.println("Sua média é: " + media);
+    }
+
+
+}
